0a2738abbaa1c9de7cc9a2539f55ce3e14610912
Tim <me@justim.net>
2024-01-25 15:52:11 +0100

    Add tests for encoding + `gzip;q=1.0` support

--- src/encoding.rs
+++ src/encoding.rs
@@ -1,3 +1,5 @@
+//! Encoding (compression) support utilities
+
 use std::convert::Infallible;
 
 use axum::async_trait;
@@ -23,13 +25,18 @@ const ENCODING_GZIP: &str = "gzip";
 /// Extension for gzip encoded files
 const ENCODING_GZIP_EXTENSION: &str = ".gz";
 
-#[derive(Clone, Copy, Debug)]
+/// Supported encodings
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Encoding {
+    /// Brotili compression
     Brotli,
+
+    /// Gzip compression
     Gzip,
 }
 
 impl Encoding {
+    /// Convert encoding to `HeaderValue`
     #[inline]
     pub const fn to_header_value(self) -> HeaderValue {
         match self {
@@ -38,6 +45,7 @@ impl Encoding {
         }
     }
 
+    /// Get extension for encoding
     #[inline]
     pub const fn get_extension(self) -> &'static str {
         match self {
@@ -47,13 +55,21 @@ impl Encoding {
     }
 }
 
+/// Client encoding support
 #[derive(Default)]
 pub struct ClientEncodingSupport {
+    /// Support for brotli encoding
     has_brotli: bool,
+
+    /// Support for gzip encoding
     has_gzip: bool,
 }
 
 impl ClientEncodingSupport {
+    /// Create new `ClientEncodingSupport` from `HeaderMap`
+    ///
+    /// Will check for `accept-encoding` header and check if it contains
+    /// `br` or `gzip` encoding
     fn from_header_map(incoming_headers: &HeaderMap) -> Self {
         let mut support = Self::default();
 
@@ -62,20 +78,31 @@ impl ClientEncodingSupport {
             .and_then(|encoding| encoding.to_str().ok())
             .map(|encoding| encoding.split(',').map(str::trim).collect::<Vec<&str>>());
 
-        support.has_brotli = Self::check_support(&encodings, ENCODING_BR);
-        support.has_gzip = Self::check_support(&encodings, ENCODING_GZIP);
+        if let Some(encodings) = encodings {
+            support.has_brotli = Self::check_support(&encodings, ENCODING_BR);
+            support.has_gzip = Self::check_support(&encodings, ENCODING_GZIP);
+        }
 
         support
     }
 
-    fn check_support(encodings: &Option<Vec<&str>>, encoding_name: &str) -> bool {
-        encodings.as_ref().map_or(false, |encodings| {
-            encodings
-                .iter()
-                .any(|encoding| encoding.eq_ignore_ascii_case(encoding_name))
+    /// Check if the client supports the given encoding
+    ///
+    /// This also checks the quality value of the encoding:
+    /// - `Accept-Encoding: deflate, gzip;q=1.0, *;q=0.5` would match `"gzip"`
+    ///
+    /// See <https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Accept-Encoding>
+    fn check_support(encodings: &[&str], encoding_name: &str) -> bool {
+        encodings.iter().any(|encoding| {
+            match encoding.split_once(';') {
+                Some((encoding, _)) => encoding.trim(),
+                None => encoding,
+            }
+            .eq_ignore_ascii_case(encoding_name)
         })
     }
 
+    /// Get list of supported encodings
     #[inline]
     pub const fn supported_encodings(&self) -> &[Encoding] {
         match (self.has_brotli, self.has_gzip) {
@@ -96,5 +123,109 @@ where
 
     async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self, Self::Rejection> {
         Ok(ClientEncodingSupport::from_header_map(&parts.headers))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_check_support() {
+        let encodings = &["gzip", "brotli"];
+
+        assert!(ClientEncodingSupport::check_support(encodings, "gzip"));
+        assert!(ClientEncodingSupport::check_support(encodings, "brotli"));
+        assert!(!ClientEncodingSupport::check_support(encodings, "deflate"),);
+    }
+
+    #[test]
+    fn test_check_support_with_quality() {
+        let encodings = &["gzip;q=1.0", "brotli"];
+
+        assert!(ClientEncodingSupport::check_support(encodings, "gzip"));
+        assert!(ClientEncodingSupport::check_support(encodings, "brotli"));
+        assert!(!ClientEncodingSupport::check_support(encodings, "deflate"),);
+    }
+
+    #[test]
+    fn test_supported_encodings() {
+        let mut support = ClientEncodingSupport::default();
+
+        support.has_gzip = true;
+        support.has_brotli = true;
+        assert_eq!(
+            support.supported_encodings(),
+            &[Encoding::Brotli, Encoding::Gzip]
+        );
+
+        support.has_gzip = false;
+        assert_eq!(support.supported_encodings(), &[Encoding::Brotli]);
+
+        support.has_brotli = false;
+        assert_eq!(support.supported_encodings(), &[]);
+
+        support.has_gzip = true;
+        assert_eq!(support.supported_encodings(), &[Encoding::Gzip]);
+    }
+
+    #[test]
+    fn test_simple_header_map() {
+        let mut headers = HeaderMap::new();
+        headers.insert(ACCEPT_ENCODING, HeaderValue::from_static("gzip, Br"));
+
+        let support = ClientEncodingSupport::from_header_map(&headers);
+
+        assert!(support.has_gzip);
+        assert!(support.has_brotli);
+
+        assert_eq!(
+            &[Encoding::Brotli, Encoding::Gzip],
+            support.supported_encodings(),
+        );
+    }
+
+    #[test]
+    fn test_header_map_with_quality() {
+        let mut headers = HeaderMap::new();
+        headers.insert(
+            ACCEPT_ENCODING,
+            HeaderValue::from_static("gzip;q=1.0, br ;  0.5"),
+        );
+
+        let support = ClientEncodingSupport::from_header_map(&headers);
+
+        assert!(support.has_gzip);
+        assert!(support.has_brotli);
+
+        assert_eq!(
+            &[Encoding::Brotli, Encoding::Gzip],
+            support.supported_encodings(),
+        );
+    }
+
+    #[test]
+    fn test_empty_header_map() {
+        let headers = HeaderMap::new();
+
+        let support = ClientEncodingSupport::from_header_map(&headers);
+
+        assert!(!support.has_gzip);
+        assert!(!support.has_brotli);
+
+        assert!(support.supported_encodings().len() == 0);
+    }
+
+    #[test]
+    fn test_simple_header_map_with_deflate() {
+        let mut headers = HeaderMap::new();
+        headers.insert(ACCEPT_ENCODING, HeaderValue::from_static("deflate"));
+
+        let support = ClientEncodingSupport::from_header_map(&headers);
+
+        assert!(!support.has_gzip);
+        assert!(!support.has_brotli);
+
+        assert!(support.supported_encodings().len() == 0);
     }
 }
